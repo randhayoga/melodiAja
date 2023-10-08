126ef53f1b099fbe616b6a3cadb59d059a7455c2
Allen <valternatean13@gmail.com>
2023-10-08 09:55:30 +0700

    perf: reduce navbar transition check rate
    Navbar now will do checking only every 100ms instead of everytime user doing scrolling

--- public/static/indexScript.js
+++ public/static/indexScript.js
@@ -1,12 +1,13 @@
 (function main() {
 	const HERO_TOP = document.getElementById("heroTop");
 	const NAVBAR = document.getElementById("navbar")
-	window.addEventListener("scroll", (ev) => {
+
+	setInterval( () => {
 		if(window.scrollY > HERO_TOP.getBoundingClientRect().bottom) {
 			NAVBAR.classList.add("navbar--withbg")
 		} else {
 			NAVBAR.classList.remove("navbar--withbg")
 		}
-		}, false
+		}, 100
 	);
 })()
