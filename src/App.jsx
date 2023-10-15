898d8a64ad40186b7d319eebb9f860b8cfeae0d3
Allen <valternatean13@gmail.com>
2023-10-16 06:33:43 +0700

    feat: Add app base

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+// import Topbar from "./components/navbar/navbar.jsx";
+// import Sidebar from "./components/sidebar/sidebar.jsx"
+// import MusicPlayer from "./components/musicPlayer/musicPlayer.jsx"
+
+// import HomePage from "./components/homePage/homePage.jsx"
+
+import "./App.css";
+
+ReactDOM.createRoot(
+	document.getElementById('root')).render(
+		<>
+			<section id="rootGrid">
+				//<Sidebar />
+				<main>
+					// <Topbar />
+					// <HomePage />
+				</main>
+			</section>
+			// <MusicPlayer />
+		</>
+)
