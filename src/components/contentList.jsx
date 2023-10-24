b920a1e17addef4879d4520fa77622778b12e336
Allen <valternatean13@gmail.com>
2023-10-24 12:18:10 +0700

    ref: change variables declarations to globals
    Others:
    fix: sidebar's height not following the content inside root

---
+++ src/components/contentList.jsx
@@ -0,0 +1,16 @@
+const contentListView = (itemList) => {
+	const render = () => {
+	}
+
+	return {render}
+}
+
+function contentList() {
+	let view = contentListView();
+
+	const render = () => {
+	}
+
+	return {render}
+}
+
