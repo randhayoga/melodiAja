ca4ee7ff79646f6807f8d6baa343284c833210e0
Allen <valternatean13@gmail.com>
2023-11-14 09:40:11 +0700

    feat: add more routes

--- backend/router.js
+++ backend/router.js
@@ -5,6 +5,25 @@ const PUBLIC_PATH = PATH.join("..", "public");
 const STATIC_PATH = PATH.join(PUBLIC_PATH, "static");
 
 (function main() {
+	/* ====================
+	POST Handlers
+	==================== */
+	ROUTER.post("/post/comments", (req, res) => {
+		console.log(req.body);
+		res.status(200).send()
+	});
+	
+	ROUTER.post("/auth/login", (req, res) => {
+		res.redirect("/discover");
+	});
+
+	ROUTER.post("/auth/register", (req, res) => {
+		res.redirect("/login");
+	});
+
+	/* ====================
+	GET Handlers
+	==================== */
 	["/discover", "/search", "/myPlaylist", "/myMusic", "/user/:id"].forEach(
 		(path) => {
 			ROUTER.get(path, (_, res) => {
