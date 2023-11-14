3e05e18b2512b49622ed271f79b60551d48abc13
Stefhan Salim <110758753+Stefhan235@users.noreply.github.com>
2023-11-14 09:47:18 +0700

    Update MelodiAja.sql

--- backend/model/MelodiAja.sql
+++ backend/model/MelodiAja.sql
@@ -1,26 +1,26 @@
 use melodiaja;
 
 create table pengguna (
-	ID_pengguna int auto_increment primary key,
+	username varchar(255) not null primary key,
     nama varchar(255) not null,
     email varchar(255) not null,
     kata_sandi varchar(255) not null,
-    file_img_profile varchar(255)
+    file_img_profile varchar(255) not null
 );
 
 create table album (
 	ID_album int auto_increment primary key,
     judul_album varchar(255),
-    file_img_album varchar(255),
+    file_img_album varchar(255) not null,
     tanggal_rilis date,
-    ID_pengguna int, 
-    foreign key (ID_pengguna) references pengguna(ID_pengguna)
+    username varchar(255), 
+    foreign key (username) references pengguna(username)
 );
 
 create table lagu (
 	ID_lagu int auto_increment primary key,
     file_musik varchar(255) not null,
-    file_img_lagu varchar(255),
+    file_img_lagu varchar(255) not null,
     judul_lagu varchar(255) not null,
     genre varchar(255) not null,
     pencipta_lagu varchar(255) not null,
@@ -28,17 +28,17 @@ create table lagu (
     jumlah_like int,
     jumlah_dislike int,
     ID_album int, 
-    ID_artist int,
+    username_artist varchar(255),
     foreign key (ID_album) references album(ID_album),
-    foreign key (ID_artist) references pengguna(ID_Pengguna)
+    foreign key (username_artist) references pengguna(username)
 );
 
 create table playlist (
 	ID_playlist int auto_increment primary key,
     judul_playlist varchar(255) not null,
-    ID_pemilik int,
-    file_img_playlist varchar(255),
-    foreign key (ID_pemilik) references pengguna(ID_pengguna)
+    username_pemilik varchar(255),
+    file_img_playlist varchar(255) not null,
+    foreign key (username_pemilik) references pengguna(username)
 );
 
 create table playlist_item (
@@ -53,8 +53,8 @@ create table komentar (
 	ID_komentar int auto_increment primary key,
     isi_komentar text,
     waktu_komentar timestamp default current_timestamp,
-    ID_pengguna int,
+    username_pengguna varchar(255),
     ID_lagu int,
-    foreign key (ID_pengguna) references pengguna(ID_pengguna),
+    foreign key (username_pengguna) references pengguna(username),
     foreign key (ID_lagu) references lagu(ID_lagu)
 );
