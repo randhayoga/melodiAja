eaa668fdcaaaa4149b975a3450a68860e2b918b9
Allen <valternatean13@gmail.com>
2023-11-16 17:57:24 +0700

    feat: add user comments
    For now, it won't be sent to server yet.

--- src/components/comments.jsx
+++ src/components/comments.jsx
@@ -1,5 +1,6 @@
 import {useState, useEffect} from "react";
 import contentList from "./contentList.jsx"
+import "./styles/comments.css"
 
 const commentsModel = (() => {
 	const fetchComments = (setter) => {
@@ -107,15 +108,87 @@ const commentsModel = (() => {
 		}, []);
 	}
 	
-	return {fetchComments}
+	const postComment = async(comment) => {
+		if(comment == "") {
+			return [-1, "No comment to send!"];
+		}
+
+		return await fetch("/post/comments", {
+				method: "POST",
+				body: JSON.stringify({
+					content: comment
+				}),
+				headers: {
+					'Content-type': 'application/json; charset=UTF-8',
+				}
+			}).then((response) => {
+				if(!response.ok) {
+					throw new Error("Network Error: Failed to send comment");
+				}
+			}).then(() => {
+				return [0, "Comment sent!"];
+			}).catch((err) => {
+				return [-1, err];
+		})
+	}
+
+	return {fetchComments, postComment}
 })()
 
 const commentsView = (() => {
 	let ContentList = contentList().render;
 
-	const render = (commentList) => {
+	const render = ({commentList, onSubmit}) => {
+		let commentDelay = 1000;
 		return (
 			<section className="commentList">
+				<section className="commentField">
+					<form className="commentForm" method="post" action="#" id="comment__form"
+						onSubmit={ async(e) => {
+							e.preventDefault();
+							
+							let commentInfo = document.getElementById("comment__info");
+							let notify = document.getElementById("comment__notification");
+							let warning = document.getElementById("comment__warning");
+							let [isSuccess, message] = await onSubmit(document.getElementById("comment__textfield").value);
+							
+							commentInfo.style.display = "block";
+							if(isSuccess == 0) {
+								notify.style.display = "block";
+								notify.textContent = message;
+							} else {
+								warning.style.display = "block";
+								warning.textContent = message;
+							}
+
+							setTimeout(() => {
+								commentInfo.style.display = "none";
+								notify.style.display = "none";
+								warning.style.display = "none";
+							}, commentDelay);
+							
+							e.target.reset()
+						}}
+					>
+						<input type="textfield" placeholder="What's in your thoughts?" id="comment__textfield"/>
+						<button type="submit" 
+							onClick={
+								(e) => {
+									e.target.style.pointerEvents = "none";
+									setTimeout(() => {
+										e.target.style.pointerEvents = "auto";
+									}, commentDelay)
+								}
+							}
+						> 
+							Post 
+						</button>
+					</form>
+					<div id="comment__info">
+						<p id="comment__notification"></p>
+						<p id="comment__warning"></p>
+					</div>
+				</section>
 				<ContentList itemList={commentList} />
 			</section>
 		)
@@ -132,7 +205,10 @@ export default (() => {
 		let [comments, setComments] = useState([]);
 		
 		model.fetchComments(setComments);
-		return view.render(comments);
+		return view.render({
+			commentList: comments,
+			onSubmit: model.postComment
+		});
 	}
 
 	return {render};
