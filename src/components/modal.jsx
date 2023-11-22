57902655c395da572a5fb2f39b6eba95a2e5d1e6
Allen <valternatean13@gmail.com>
2023-11-23 06:00:59 +0700

    fix: modal not covering the musicPlayer
    others:
    feat: add interface to toggle the modal

--- src/components/modal.jsx
+++ src/components/modal.jsx
@@ -2,44 +2,48 @@ import "./styles/modal.css"
 import {useState} from "react";
 
 const modalView = () => {
-	const render = (status, {id, heading}, {Component}) => {
-		return ( status.get == true? (
-			<section id={`${id}Dialog`}>
-				<div className="dialog__box">
-					<div className="dialog__top">
-						<h3 className="dialog__name"> {heading} </h3>
-						<div
-							onClick = {() => status.set(false)}
+	const toggle = (id) => {
+		document.getElementById(id).classList.toggle("modal__box--hidden");
+	}
+
+	const render = ({id, heading}, Component) => {
+		return (
+			<section id={`${id}Modal`}>
+				<div className="modal__box">
+					<div className="modal__top">
+						<h3 className="modal__name"> {heading} </h3>
+						<div className="icon icon--tiny"
+							onClick={() => toggle(`${id}Modal`)}
 						>
-							<div className="icon icon--tiny">
-								<img src="/icons/close_x.png" alt="close icon" />
-							</div>
+							<img src="/icons/close_x.png" alt="close icon" />
 						</div>
 					</div>
-					<div className="dialog__body">
+					<div className="modal__body">
 						<Component />
 					</div>
 				</div>
-				<div className="dialog__shade" > </div>
+				<div className="modal__shade" > </div>
 			</section>
-			): <></>
 		)
 	}
 	
-	return {render};
+	return {render, toggle};
 }
 
 export default function modal(modalID, modalName) {
 	let view = modalView();
-	let props = {
+	let config = {
 		id: modalID,
 		heading: modalName,
 	};
 
-	const render = (content) => {
-		const [isShowing, setShowing] = useState(true);
-		return view.render({get: isShowing, set: setShowing}, props, content);
+	const render = ({component}) => {
+		return view.render(config, component);
 	}
 
-	return {render};
+	const toggle = () => {
+		view.toggle(config.id + "Modal");
+	}
+	
+	return {render, toggle};
 }
